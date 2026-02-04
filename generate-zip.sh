e4f048bb064f3c15136981ef3d2dc7816c8c4fc4
Daniel Vijge <danielvijge@users.noreply.github.com>
2026-02-04 22:34:36 +0100

    Append -local to build version to distinguish from GitHub beta version

--- generate-zip.sh
+++ generate-zip.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -x
 
-VERSION=`git describe --tags --abbrev=0`.`git rev-list $(git describe --tags --abbrev=0)..HEAD --count`
+VERSION=`git describe --tags --abbrev=0`.`git rev-list $(git describe --tags --abbrev=0)..HEAD --count`-local
 
 sed "s/{{ env\['VERSION'\] }}/$VERSION/g" install.template.xml > install.xml
 zip -r SqueezeCloud-$VERSION.zip . -x \*.zip \*.sh \*.git\* \*README\* \*webauth\* \*.sublime\* \*.DS_Store\* \*.editorconfig \*.template.xml
